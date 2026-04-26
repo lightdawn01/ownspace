39e13a57b4719040fdb94c8fd5dceef336a4eded
lightdawn01 <166994066+lightdawn01@users.noreply.github.com>
2026-04-26 17:35:46 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
  const todayDate = dayjs().format('dddd, MMMM D, YYYY');
-document.getElementById('today-time').textContent = todayDate;
+document.getElementById('today-date').textContent = todayDate;
+
+const todayTime = dayjs().format('mm');
+document.getElementById('today-time').textContent = todayTime;
+
